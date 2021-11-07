ef94f77aa97be26ac7e631e053967e3ebb94fe38
aadit-patil <aaditpatil99@gmail.com>
2021-11-07 14:11:32 +0530

    DevChap2-#P!

--- lib/main.dart
+++ lib/main.dart
@@ -18,12 +18,45 @@ class HomePage  extends StatelessWidget {
          appBar: AppBar(
            title: Text("Awesome App"),
          ),
-         body: Container(
-           child: Center(child: Text ("Hii Flutter: Aadits First App"))
+         body: Center(
+           child: Container(
+             width: 100,
+           height: 100,
+           padding: const EdgeInsets.all(8),
+           color:Colors.red,
+          //  alignment: Alignment.center,
+          //  color: Colors.teal,
+           
+        //    child: Text("I  Box",
+        //    style: TextStyle(
+        //      color: Colors.red,
+        //     fontSize: 20,
+        //     ),
+        // ),
+        
+           
+        //    decoration: BoxDecoration(
+        //     //  shape: BoxShape.circle,
+        //      borderRadius: BorderRadius.circular(10),
+        //      boxShadow: [BoxShadow(
+        //        color:Colors.grey,
+        //        blurRadius:6,
+               
+        //       //  spreadRadius: 10,
+        //       offset: Offset(2.0,10.0),
+        //        )],
+
+        //      color: Colors.teal,
+        //      gradient: LinearGradient(colors: [ Colors.yellow,Colors.green]),
+        //    ),
+        //  )
          ),
-       ),
-      
+       )
+       )
     );
+
+      
+    
   }
 }
 
